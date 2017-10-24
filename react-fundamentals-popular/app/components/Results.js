c3840accde05618f06192f8d494c43bd7cbef18a
ganchimeg <ganchimeg10@gmail.com>
2017-10-24 18:07:04 -0400

    final changes?

---
+++ react-fundamentals-popular/app/components/Results.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var queryString = require('query-string');
+
+class Results extends React.Component {
+  render() {
+	var players = queryString.parse(this.props.location.search)
+	console.log(players);
+    return (
+      <div>Results!</div>
+    )
+  }
+}
+
+module.exports = Results;
